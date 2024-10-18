4a7d060bcf3c4a23fc8d556c3de5e917fae21f28
harshpandita2000 <panditaharsh32@gmail.com>
2024-10-18 10:44:05 +0530

    ignore

---
+++ day4/HRMS/Common/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int Id,string Name,int Age,double Salary)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+            this.Salary = Salary;
+            
+
+        }
+    }
+}
---
+++ day4/HRMS/Services/Employee/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Employee
+{
+    public class EmployeeService
+    {
+        public EmployeeService()
+        { 
+        
+            public IEnumerable<Employee> Get()
+            {
+
+
+            }
+        }
+    }
+}
